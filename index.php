9a88885c7dcf2695d9fcbfd892168d6b6cdb4ec7
and3407 <and3407@yandex.ru>
2023-03-21 16:33:27 +0400

    add task controller

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
 <?php
 
 use LeadHandler\controllers\StartController;
+use LeadHandler\controllers\TaskController;
 
 $container = require __DIR__ . '/app/bootstrap.php';
 
 switch ($argv[1]) {
     case 'start':
         $container->call(StartController::class);
+        break;
+    case 'task':
+        $container->call(TaskController::class);
         break;
     default:
         die();
